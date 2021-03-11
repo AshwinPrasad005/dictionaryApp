2386dc9982beea35d248e1c372bfeb06cca28cf3
AshwinPrasad005 <64143870+AshwinPrasad005@users.noreply.github.com>
2021-03-11 11:44:34 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './Screens/HomeScreen.js';
+import { createAppContainer, createSwitchNavigator} from 'react-navigation'; 
+
+export default class App extends React.Component {
+  render(){
+    return (
+      <View style={styles.container}>
+        <AppContainer/>
+      </View>
+    );
+  }
+}
+
+var AppNavigator = createSwitchNavigator({
+  HomeScreen:HomeScreen,
+})
+
+const AppContainer = createAppContainer(AppNavigator)
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
---
+++ Screens/HomeScreen.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import { StyleSheet, Text, View , TextInput , TouchableOpacity } from 'react-native';
+import { Header } from 'react-native-elements';
+import dictionary from '../database.js';
+
+export default class HomeScreen extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            text : "",
+            wordSearched : "",
+            wordFromDatabase : "",
+            lexicalCategory : "",
+            definition : "",
+            isButtonPressed : "",
+        }
+    }
+    getWord=(text)=>{
+        var text = text.toLowerCase();
+
+        try{
+            var word = dictionary[text]["word"]
+            var lexicalCategory = dictionary[text]["lexicalCategory"]
+            var definition = dictionary[text]["definition"]
+            this.setState({
+                "word" : word,
+                "lexicalCategory" : lexicalCategory,
+                "definition" : definition
+            })
+        }
+        catch(err){
+            alert("Sorry , this word is not available right now");
+            this.setState({
+                "text" : "",
+                "isButtonPressed" : false,
+            })
+        }    
+    }
+    render(){
+        return(
+            <View style = {styles.container}>
+                <Header centerComponent={{ text : "DICTIONARYAPP" , style: { color : '#fff' } }} />
+                <TextInput 
+                style={styles.inputBox} 
+                onChangeText={text => {
+                    this.setState({
+                        text: text,
+                        isSearchPressed:false,
+                        word: "Loading...",
+                        lexicalCategory : "",
+                        examples : [],
+                        defination : "",
+                    })
+                }}
+                />
+                <TouchableOpacity 
+                    style = {styles.searchButton} 
+                    onPress = {(()=>{
+                        this.setState({ isSearchPressed : true })
+                        this.getWord(this.state.text);
+                    })}>
+                    <Text>Search</Text>
+                </TouchableOpacity>
+                    <Text style = {styles.title}>Word : {""}</Text>
+                    <Text style = {{ fontSize : 18 , marginTop : -30 , marginLeft : 150}}>{this.state.word}</Text>
+                    <Text style = {styles.title}>Type : {""}</Text>
+                    <Text style = {{ fontSize : 18 , marginLeft : 100 }}>{this.state.lexicalCategory}</Text>
+                    <Text style = {styles.title}>Definition : {""}</Text>
+                    <Text style = {{ fontSize : 18 , marginLeft : 30 }}>{this.state.defination}</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex : 1
+    },
+    title:{
+        color: 'black',
+        marginTop: 20,
+        marginLeft: 30,
+        fontSize : 30
+    },
+    inputBox:{
+        width:"75%", 
+        height:35, 
+        alignSelf:'center', 
+        borderColor:'#ffab91', 
+        borderRadius:10, 
+        borderWidth:1, 
+        marginTop:20, 
+        padding:10, 
+    },
+    searchButton:{ 
+        width:"75%", 
+        height:40, 
+        justifyContent:'center', 
+        alignItems:'center', 
+        borderRadius:10, 
+        backgroundColor:"#ff5722", 
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 8,
+            },
+        marginLeft : 45,
+        marginTop : 10    
+        }        
+})
